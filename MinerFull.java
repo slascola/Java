34d2b962536bbe68cbbf34b3b7495c68369df994
Stephanie <slascola@calpoly.edu>
2015-04-26 20:24:59 -0700

    Added MinerFull class with methods

---
+++ MinerFull.java
@@ -0,0 +1,21 @@
+
+public class MinerFull extends Miner
+{
+
+   private double current_img;
+   private double resource_count;
+   public MinerFull(Entity name, Dudes resource_limit, Entity position, 
+		      Dudes rate, Entity imgs, Miner animation_rate)
+   {
+	   super(name, resource_limit, position, rate, imgs, animation_rate);
+	   this.current_img = 0;
+	   this.resource_count = resource_limit;
+   }
+   public MinerNotFull try_transform_miner_full(WorldModel world)
+   {
+	   MinerNotFull new_entity = new MinerNotFull(this.get_name(), 
+			   this.get_resource_limit(), this.get_position(),
+			   this.get_rate(), this.get_images(), this.get_animation_rate());
+	   return new_entity;
+   }
+}
