0448fb34003319cb7501e42000443c6690d627f9
Stephanie <slascola@calpoly.edu>
2015-04-29 16:53:22 -0700

    Changed Types in Blacksmith.java

--- Blacksmith.java
+++ Blacksmith.java
@@ -1,12 +1,12 @@
 
 public class Blacksmith extends Dudes
 {
-   private double resource_count;
-   private double resource_distance;
-   public Blacksmith(Entity name, Entity position, Dudes resource_limit, Dudes rate, double resource_distance)
+   private int resource_count;
+   private int resource_distance;
+   public Blacksmith(String name, Point position, int resource_limit, int rate, int resource_distance)
    {
 	   super(name, position, rate, resource_limit);
-	   this.resource_count = 0.0;
+	   this.resource_count = 0;
 	   this.resource_distance = resource_distance;
 	   
    }
