1cec5b08a9a9546a49e8cbf2cec3566ceb8d17e7
Stephanie <slascola@calpoly.edu>
2015-04-29 00:10:32 -0700

    Added Obstacle class

---
+++ Obstacle.java
@@ -0,0 +1,12 @@
+
+public class Obstacle extends Entity
+{
+   public Obstacle(Entity name, Entity position)
+   {
+	   super(name, position);
+   }
+   protected String entity_string()
+   {
+	   //help
+   }
+}
