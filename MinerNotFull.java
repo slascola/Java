334e689c01d7950ffe0c744b7a4550dd3f8104e3
Stephanie <slascola@calpoly.edu>
2015-04-29 16:55:37 -0700

    Changed types in MinerNotFull class

--- MinerNotFull.java
+++ MinerNotFull.java
@@ -4,16 +4,16 @@ public class MinerNotFull extends Miner
 
 	private int current_img;
 	private int resource_count;
-	   public MinerNotFull(Entity name, Dudes resource_limit, Entity position, 
-	      Dudes rate, Entity imgs, Miner animation_rate)
+	   public MinerNotFull(String name, int resource_limit, Point position, 
+	      int rate, String imgs, int animation_rate)
 	   {
-		  super(name, resource_limit, position, rate, imgs, animation_rate);
+		  super(name, position, rate, resource_limit, animation_rate);
 	      this.current_img = 0;
 		  this.resource_count = 0;
 	   }
 	   public String entity_string()
 	   {
-		   String miner_string = new String();
+		   String miner_string;
 		   miner_string = "miner" + this.name + toString(this.position.x) +
 		   toString(this.position.y) + toString(this.resource_limit) + 
 		   toString(this.rate) + toString(this.animation_rate);
@@ -38,7 +38,7 @@ public class MinerNotFull extends Miner
 	   
 	   protected boolean miner_to_ore(WorldModel world, Ore ore)
 	   {
-		   Point entity_pt = 
+		   Point entity_pt = this.get_position();
 		   
 	   }
 }
