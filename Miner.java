f6ac4cd719f3ae486f0baf1760fb99aaa6569a11
Stephanie <slascola@calpoly.edu>
2015-04-29 16:54:46 -0700

    Changed types in Miner class

--- Miner.java
+++ Miner.java
@@ -1,12 +1,12 @@
-//Object declaration original like String or from class
+
 public class Miner extends Dudes
 {
-   private double resource_count;
-   private double resource_limit;
-   public Miner(Entity name, Dudes resource_limit, Entity position, Dudes rate)
+   private int resource_count;
+   private int resource_limit;
+   public Miner(String name, Point position, int rate, int resource_limit, int resource_count)
    {
-	   super(name, position, rate, resource_limit);
-	   this.resource_count = 0.0;
+	   super(name, position, rate, resource_limit, resource_count);
+	   this.resource_count = 0;
 	   this.resource_limit = resource_limit;
    }
    
