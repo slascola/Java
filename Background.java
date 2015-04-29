705b70ef4cb759c6b9527ad3b253ee5d7e43fe49
Stephanie <slascola@calpoly.edu>
2015-04-29 00:06:40 -0700

    Added Background class

---
+++ Background.java
@@ -0,0 +1,14 @@
+
+public class Background 
+{
+   private String name;
+   public Background(String name)
+   {
+	   this.name = name;
+	   
+   }
+   protected String get_name()
+   {
+	   return this.name;
+   }
+}
