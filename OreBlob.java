c510d2c6a669d8023d7e3a8f8e37bd8514ca2a98
Stephanie <slascola@calpoly.edu>
2015-04-29 00:11:39 -0700

    Added OreBlob class

---
+++ OreBlob.java
@@ -0,0 +1,12 @@
+
+public class OreBlob extends Rate
+{
+   public OreBlob(Entity name, Entity position, Rate rate)
+   {
+	   super(name, position, rate);
+   }
+   protected boolean blob_to_vein(WorldModel world, Vein vein)
+   {
+	   //ask for help
+   }
+}
