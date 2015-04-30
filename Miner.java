94fbe4c35d8a331129a36a42ddac1ec4008c36d9
Stephanie <slascola@calpoly.edu>
2015-04-30 11:42:31 -0700

    Fixed a few errors in Miner class

--- Miner.java
+++ Miner.java
@@ -3,11 +3,17 @@ public class Miner extends Dudes
 {
    private int resource_count;
    private int resource_limit;
-   public Miner(String name, Point position, int rate, int resource_limit, int resource_count)
+   private int animation_rate;
+   public Miner(String name, Point position, int rate, int resource_limit, int resource_count, int animation_rate)
    {
 	   super(name, position, rate, resource_limit, resource_count);
 	   this.resource_count = 0;
 	   this.resource_limit = resource_limit;
+	   this.animation_rate = animation_rate;
    }
    
+   protected int get_animation_rate()
+   {
+	  return this.animation_rate;
+   }
 }
