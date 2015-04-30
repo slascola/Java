585e9eff2fb04aa962b0201ea34578ac04592cb3
Stephanie <slascola@calpoly.edu>
2015-04-30 11:42:00 -0700

    Fixed a few errors with adding methods still needs changes Blacksmith

--- Blacksmith.java
+++ Blacksmith.java
@@ -3,15 +3,19 @@ public class Blacksmith extends Dudes
 {
    private int resource_count;
    private int resource_distance;
-   public Blacksmith(String name, Point position, int resource_limit, int rate, int resource_distance)
+   public Blacksmith(String name, Point position, int resource_limit, int rate, int resource_distance, int resource_count)
    {
-	   super(name, position, rate, resource_limit);
+	   super(name, position, rate, resource_limit, resource_count);
 	   this.resource_count = 0;
-	   this.resource_distance = resource_distance;
+	   this.resource_distance = 1;
 	   
    }
    protected String entity_string()
    {
-	   return 
+	   String blacksmithString;
+	   blacksmithString = "blacksmith" + this.get_name() + toString(this.get_position()) 
+			   + toString(this.get_resource_limit()) +
+			   toString(this.get_rate()) + toString(this.resource_distance);
+	   return blacksmithString;
    }
 }
