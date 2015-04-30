da983955be7a99ab6ee71d173f7e9d0a9f72e52f
Stephanie <slascola@calpoly.edu>
2015-04-30 11:44:37 -0700

    Fixed a few errors in Oreblob class

--- OreBlob.java
+++ OreBlob.java
@@ -1,12 +1,38 @@
 
 public class OreBlob extends Rate
 {
-   public OreBlob(String name, Point position, int rate)
+	private int animation_rate;
+   public OreBlob(String name, Point position, int rate, int animation_rate)
    {
 	   super(name, position, rate);
+	   this.animation_rate = animation_rate;
+   }
+   protected int get_animation_rate()
+   {
+	   return this.animation_rate;
    }
    protected boolean blob_to_vein(WorldModel world, Vein vein)
    {
-	   //ask for help
+	   Point entity_pt = this.get_position();
+	   if(! vein)
+	   {
+		   return false;
+	   }
+	   Point vein_pt = vein.get_position();
+	   if (actions.adjacent(entity_pt, vein_pt))
+	   {
+		   actions.remove_entity(vein, world);
+		   return true;
+	   }
+	   else
+	   {
+		   Point new_pt = actions.blob_next_position(world, entity_pt, vein_pt);
+		   Entity old_entity = world.get_title_occupant(new_pt);
+		   if (old_entity instanceOf(Ore)
+           {
+			 actions.remove_entity(old_entity, world);
+		   }
+		   return false; 
+	   }
    }
 }
