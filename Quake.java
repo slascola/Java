1a0e044fe9e140f639acbfccb3e09b0da1b864cc
Stephanie <slascola@calpoly.edu>
2015-04-30 11:45:03 -0700

    Fixed a few errors and added methods in Quake class

--- Quake.java
+++ Quake.java
@@ -1,5 +1,15 @@
 
 public class Quake extends Entity
 {
-   
+	private int animation_rate;
+   public Quake(String name, Point position, int animation_rate)
+   {
+	   super(name, position);
+	   this.animation_rate = animation_rate;
+	   
+   }
+   protected int get_animation_rate()
+   {
+	   return this.animation_rate;
+   }
 }
