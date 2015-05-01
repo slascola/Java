3c656bee03c5f60abb1650b0070fb0c65bd3d9c7
Stephanie <slascola@calpoly.edu>
2015-05-01 16:00:02 -0700

    FINALLY FIXED STRINGS Ore class

--- Ore.java
+++ Ore.java
@@ -4,14 +4,14 @@ public class Ore extends Rate
    private int rate;
    public Ore(String name, Point position, int rate)
    {
-	   super(name, position, rate); //redefining rate?
+	   super(name, position, rate); 
 	   this.rate = 5000;
    }
    protected String entity_string()
    {
 	   String ore_string;
-	   ore_string = "ore" + this.get_name() + String.valueOf(this.get_position()) + 
-			   Integer.toString(this.get_rate());
+	   ore_string = "ore" + " " + this.get_name() + " " + this.get_position().x + " " 
+			  + this.get_position().y + " " + (this.get_rate());
 	   return ore_string;
    }
 }
