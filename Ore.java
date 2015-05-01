1d8ccec4c94983ac1fc888f064c6f0dcdd36e361
Stephanie <slascola@calpoly.edu>
2015-04-30 20:26:40 -0700

    Fixed entity string syntax for Ore class

--- Ore.java
+++ Ore.java
@@ -10,7 +10,8 @@ public class Ore extends Rate
    protected String entity_string()
    {
 	   String ore_string;
-	   ore_string = "ore" + this.get_name() + this.get_position() + this.get_rate();
+	   ore_string = "ore" + this.get_name() + String.valueOf(this.get_position()) + 
+			   Integer.toString(this.get_rate());
 	   return ore_string;
    }
 }
