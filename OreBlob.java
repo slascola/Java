489a4f0e2fa235c8b6659951b9a4a13046da83b0
Stephanie <slascola@calpoly.edu>
2015-05-01 18:48:09 -0700

    Fixed Actions class functions in OreBlob class and added move entity

--- OreBlob.java
+++ OreBlob.java
@@ -19,19 +19,20 @@ public class OreBlob extends Rate
 		   return false;
 	   }
 	   Point vein_pt = vein.get_position();
-	   if (actions.adjacent(entity_pt, vein_pt))
+	   if (Actions.adjacent(entity_pt, vein_pt))
 	   {
-		   actions.remove_entity(vein, world);
+		   Actions.remove_entity(vein, world);
 		   return true;
 	   }
 	   else
 	   {
-		   Point new_pt = actions.blob_next_position(world, entity_pt, vein_pt);
+		   Point new_pt = Actions.blob_next_position(world, entity_pt, vein_pt);
 		   Entity old_entity = world.get_title_occupant(new_pt);
 		   if (old_entity instanceof Ore)
            {
-			 actions.remove_entity(old_entity, world);
+			 Actions.remove_entity(old_entity, world);
 		   }
+		   world.move_entity(old_entity, world);
 		   return false; 
 	   }
    }
