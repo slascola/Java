e1ce3b358dfc464e1b9ecc52d4f098cb9355d899
Stephanie <slascola@calpoly.edu>
2015-05-04 20:58:56 -0700

    Added next position and blob next position to Actions

--- Actions.java
+++ Actions.java
@@ -19,11 +19,58 @@ import java.lang.Math;
       }
      public static Point next_position(WorldModel world, Point entity_pt, Point dest_pt)
      {
-    	 return null;
+    	int pt = dest_pt.x - entity_pt.x;
+    	double horiz = Math.signum(pt);
+    	int horiz2 = (int)horiz;
+        int x = entity_pt.x + horiz2;
+        int y = entity_pt.y;
+    	Point newpoint = new Point(x, y);
+    	if (horiz == 0 || WorldModel.is_occupied(world, newpoint))
+    	{
+    		int y1 = dest_pt.y - entity_pt.y;
+    		double vert = Math.signum(y1);
+    		int vert2 = (int)vert;
+    		int newx = entity_pt.x;
+    		int newy = entity_pt.y  + vert2;
+    		newpoint = new Point(newx, newy);
+    		if (vert2 == 0 || WorldModel.is_occupied(world, newpoint))
+    		{
+    			int fx = entity_pt.x;
+    			int fy = entity_pt.y;
+    			newpoint = new Point(fx, fy);
+    		}
+    	}
+    	
+    	return newpoint;
+    	
      }
      public static Point blob_next_position(WorldModel world, Point entity_pt, Point dest_pt)
      {
-    	return null; 
+    	int pt = dest_pt.x - entity_pt.x;
+     	double horiz = Math.signum(pt);
+     	int horiz2 = (int)horiz;
+         int x = entity_pt.x + horiz2;
+         int y = entity_pt.y;
+     	Point newpoint = new Point(x, y);
+     	if (horiz == 0 || WorldModel.is_occupied(world, newpoint) && (world.get_tile_occupant(newpoint) instanceof Ore == false))
+     	{
+     		int y1 = dest_pt.y - entity_pt.y;
+     		double vert = Math.signum(y1);
+     		int vert2 = (int)vert;
+     		int newx = entity_pt.x;
+     		int newy = entity_pt.y  + vert2;
+     		newpoint = new Point(newx, newy);
+     		if (vert2 == 0 || WorldModel.is_occupied(world, newpoint) && (world.get_tile_occupant(newpoint) instanceof Ore == false))
+     		{
+     			int fx = entity_pt.x;
+     			int fy = entity_pt.y;
+     			newpoint = new Point(fx, fy);
+     		}
+     	}
+     	
+     	return newpoint;
+     	
+    	
      }
      public static void remove_entity(Entity entity, WorldModel world)
      {
