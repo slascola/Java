93c09d339a5ff5fe9bb9e123cc25bacede89e07d
Stephanie <slascola@calpoly.edu>
2015-05-04 21:00:42 -0700

    Added blob to vein method in OreBlob

--- OreBlob.java
+++ OreBlob.java
@@ -11,11 +11,11 @@ public class OreBlob extends Rate
    {
 	   return this.animation_rate;
    }
-   /*
+   
    protected boolean blob_to_vein(WorldModel world, Vein vein)
    {
 	   Point entity_pt = this.get_position();
-	   if(vein != null)
+	   if(vein == null)
 	   {
 		   return false;
 	   }
@@ -28,14 +28,14 @@ public class OreBlob extends Rate
 	   else
 	   {
 		   Point new_pt = Actions.blob_next_position(world, entity_pt, vein_pt);
-		   Entity old_entity = world.get_title_occupant(new_pt);
+		   Entity old_entity = world.get_tile_occupant(new_pt);
 		   if (old_entity instanceof Ore)
            {
 			 Actions.remove_entity(old_entity, world);
 		   }
-		   world.move_entity(old_entity, world);
+		   world.move_entity(old_entity, entity_pt);
 		   return false; 
 	   }
    }
-  */ 
+   
 }
