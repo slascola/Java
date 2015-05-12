b5408fffc8a98479e7f31b79cea1acedcd91cff1
Stephanie <slascola@calpoly.edu>
2015-05-12 01:05:44 -0700

    Added imgs methods to Background

--- Background.java
+++ Background.java
@@ -2,13 +2,30 @@
 public class Background 
 {
    private String name;
-   public Background(String name)
+   private int[] imgs;
+   private int current_img;
+   public Background(String name, int []imgs)
    {
 	   this.name = name;
+	   this.imgs = imgs;
+	   this.current_img = 0;
+	   
 	   
    }
    protected String get_name()
    {
 	   return this.name;
+   }
+   protected int[] get_images()
+   {
+	   return this.imgs;
+   }
+   protected int get_image()
+   {
+	   return this.imgs[this.current_img];
+   }
+   protected void next_image()
+   {
+	   this.current_img = (this.current_img + 1) % this.imgs.length; 
    }
 }
